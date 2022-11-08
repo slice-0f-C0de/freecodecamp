9ad257f06da3725e6f7a5c62b4b0f2021ed79ab2
slice_of_code <slice_of_code@mail.ru>
2022-11-08 18:38:31 +0400

    Manipulating Complex Objects

---
+++ 91.js
@@ -0,0 +1,21 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true,
+      },
+      {"artist": "Linkin Park",
+      "title": "Minutes to Midnight",
+      "release_year": 2007,
+      "formats": [
+        "LP",
+        "CD",
+        "Vinyl"
+      ]}
+  ];
