c9ebedbbce2a3930d5b32ca358fafa455d4bf689
slice_of_code <slice_of_code@mail.ru>
2022-11-10 19:06:01 +0400

    Create a Module Script

--- ES6/16.js
+++ ES6/16.js
@@ -1,10 +1,9 @@
-// Only change code below this line
-const bicycle = {
-    gear: 2,
-    setGear(newGear) {
-      this.gear = newGear;
-    }
+const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {
+      name,
+      age,
+      gender
+    };
+    // Only change code above this line
   };
-  // Only change code above this line
-  bicycle.setGear(3);
-  console.log(bicycle.gear);
