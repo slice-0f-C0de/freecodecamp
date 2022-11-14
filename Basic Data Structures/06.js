3ad655757099d80a2215f06ee106cb7b5aa0e65f
slice_of_code <slice_of_code@mail.ru>
2022-11-14 11:55:02 +0400

    Add Items Using splice()

---
+++ Basic Data Structures/06.js
@@ -0,0 +1,10 @@
+function htmlColorNames(arr) {
+    // Only change code below this line
+    const startIndex = 0;
+    const amountToDelete = 2;
+    arr.splice(startIndex, amountToDelete, "DarkSalmon", "BlanchedAlmond");
+    // Only change code above this line
+    return arr;
+  }
+  
+  console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
